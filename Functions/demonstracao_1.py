1ef21458ce2ce048f09f8950442320d0eb36d282
LyndainesSantos <lyndainessantos@gmail.com>
2023-08-16 01:07:10 -0300

    Add function code

--- Functions/demonstracao_1.py
+++
@@ -1,8 +0,0 @@
-def calcular_media(nota1:str, nota2:dict) -> dict:
-    '''Retorna uma saudação personalizada '''
-    media = (nota1 + nota2)/2
-    situacao = "Aprovado" if media >= 7 else "Reprovado"
-    return media, situacao
-
-for media, situacao in [calcular_media(7,8)]:
-    print(media, situacao)
